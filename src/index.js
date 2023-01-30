4342b9a889b73d5c3a1343ad14844a68615c3b99
Alex <arodmesa@gmail.com>
2023-01-30 23:47:16 +0100

    css font awesome optimized and changed BrowserRouter by HashRouter

--- src/index.js
+++ src/index.js
@@ -5,15 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store';
 import { Provider } from 'react-redux';
-import {BrowserRouter} from 'react-router-dom'
+//import {BrowserRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </Provider>  
 );
